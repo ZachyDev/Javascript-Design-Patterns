a1567cbb861681b565581bbee594b317857eb4ca
ZachyDev <mosetizachary001@gmail.com>
2020-06-13 04:26:31 +0300

    JS design patterns

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-const users = Object.create(Object.prototype);
-users.name = 'Zachy';
-console.log(users.name)
+// const users = {
+//     name: 'Zachy',
+//     age: 20
+// };
+
+// Object.defineProperty(users,'name',{ writable: false })
+// users.name = 'hello'
+// console.log(users.name)
+
+const users = ['zachy','finna','doroh'];
+Object.defineProperty(Array.prototype,'last',{get: function() {
+    return this[this.length-1]
+}})
+const companies = ['saf','codeln'];
+console.log(users.last)
+console.log(companies.last);
